165701cbbe1975107a6b9320193aa5cf2c236f2c
yairgorenn <yairgorenn@gmail.com>
2026-08-09 22:47:43 +0300

    timeout 25 sec

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ rdb = redis.from_url(REDIS_URL, decode_responses=True)
 # =========================
 # CONFIG
 # =========================
-TASK_TTL = 25        # Max lifetime of a task (seconds)
-CLIENT_TIMEOUT = 20  # Client-side timeout (seconds)
+TASK_TTL = 35        # Max lifetime of a task (seconds)
+CLIENT_TIMEOUT = 25  # Client-side timeout (seconds)
 RESULT_TTL = 10      # Result retention time (seconds)
 
 K_TASK   = "gate:task"
